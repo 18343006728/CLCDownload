1da639400f3c013e62279d5717d848b4c01a0252
mac13 <365069295@qq.com>
2021-05-24 11:53:08 +0800

    Initial Commit of Download SDK
    Signed-off-by: mac13 <365069295@qq.com>

---
+++ CLCDownload/Classes/TestViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TestViewController.swift
+//  CLCDownload
+//
+//  Created by xin wang on 2021/5/24.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("HELLO WORLD")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
